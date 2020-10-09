9e5a53255ff93c11e43b1c9117d29a7f9d3c37e8
Arnav <arnavbiswas2000@gmail.com>
2020-10-10 01:29:26 +0530

    line col no , curr pos added ! cmt-2

--- shell.py
+++ shell.py
@@ -4,7 +4,7 @@ running = True
 
 while running:
     text = input('$basic > ')
-    result, error = basic.run(text)
+    result, error = basic.run('<stdin>', text)
 
     if text == 'exit!':
         break
