a64b00a90dcf144ceb4a28d002f16236ccfad270
Arnav <arnavbiswas2000@gmail.com>
2020-10-10 18:24:42 +0530

    BinOpNode added , working on Parser class

--- basic.py
+++ basic.py
@@ -148,6 +148,79 @@ class Lexer:
         else:
             return Token(TT_FLOAT, float(num_str))
 
+#################################
+########### NODES ###############
+#################################
+
+
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+    def __repr__(self):
+        return f'{self.tok}'
+
+
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+
+    def __repr__(self):
+        return f'{self.left_node}, {self.op_tok}, {self.right_node}'
+
+#################################
+##########  PARSER  #############
+#################################
+
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = 1
+        self.advance()
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+        left = self.current_tok
+        self.advance()
+
+        while self.current_tok in (TT_DIV, TT_MUL):
+            op_tok = self.current_tok
+            self.advance()
+            right = self.current_tok
+            left = BinOpNode(left, op_tok, right)
+
+        return left
+
+    def expr(self):
+        pass
+
+    # def binaryOp(self, func, ops):
+    #     left = self.current_tok
+    #     self.advance()
+
+    #     while self.current_tok in (TT_DIV, TT_MUL):
+    #         op_tok = self.current_tok
+    #         self.advance()
+    #         right = self.current_tok
+    #         left = BinOpNode(left, op_tok, right)
+
+    #     return left
+
 
 #################################
 ##########  RUN  ################
